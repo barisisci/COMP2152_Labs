469a31d5a6dffe64de60a34f9489c3cc1fe728d6
barisisci <devbaris.isci@gmail.com>
2025-01-31 08:57:49 -0500

    Q3

--- week4/lab_week4.py
+++ week4/lab_week4.py
@@ -80,17 +80,14 @@ for weapon in weapons:
 loot_options = ["Health Potion", "Poison Potion", "Secret Note", "Leather Boots", "Flimsy Gloves"]
 good_loot_options = ["Health Potion", "Leather Boots"]
 bad_loot_options = ["Poison Potion"]
-#Lab04 - Q1
-#Define the Monster Power
 
+# Lab04 - Q1
+# Define the Monster Power
 monster_power = {
     "Fire Magic": 2,
     "Freezing Time": 4,
     "Super Hearing": 6,
 }
-
-
-
 
 # Define the number of stars awarded to the Player
 num_stars = 0
@@ -162,10 +159,15 @@ print("--- You are matched in strength: " + str(combat_strength == m_combat_stre
 # Check the Player's overall strength and health
 print("--- You have a strong player: " + str((combat_strength + health_points) >= 15))
 
-#Lab04 - Q2
-#Roll for the Monster's Power
+# Lab 04 - Q2
+# Roll for the Monster's Power
 input("Roll for the Monster's Magic Power (Press Enter)")
-power_roll = random.choice(["Fire Magic","Freezing Time","Super Hearing"])\
+power_roll = random.choice(["Fire Magic", "Freezing Time", "Super Hearing"])
+
+# Lab 04 - Q3
+# Increase the Monster Combat Strength by it's Power
+m_combat_strength = min(6, m_combat_strength + monster_power[power_roll])
+print("The Monster Combat Strength is no "+ str(combat_strength) + "Using the " + power_roll + " magic power.")
 
 # Loop while the monster and the player are alive. Call fight sequence functions
 print("You meet the monster. FIGHT!!")
